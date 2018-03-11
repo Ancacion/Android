2b29bba7fd4126def793cd40c57319601b90d1e8
Ancacion <joker103186@gmail.com>
2018-03-11 16:59:09 +0800

    1st homework for app

--- app/src/main/java/com/example/user/hw1/MainActivity.java
+++ app/src/main/java/com/example/user/hw1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.hw1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +23,35 @@ public class MainActivity extends AppCompatActivity {
         minputage = (EditText) findViewById(R.id.editText2);
         mok = (Button) findViewById(R.id.button);
         mtxtR = (TextView) findViewById(R.id.textView2);
+
+        mok.setOnClickListener(buttonOnClick);
     }
+
+    private View.OnClickListener buttonOnClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String strSex = minputsex.getText().toString();
+            int Age = Integer.parseInt(minputage.getText().toString());
+
+            String strSug = getString(R.string.txtR);
+            if (strSex.equals(getString(R.string.sex_male)))
+                if(Age < 30)
+                    strSug += getString(R.string.sug_not_hurry);
+                 else if(Age >= 30 && Age <= 35)
+                     strSug += getString(R.string.sug_get_married);
+                 else
+                    strSug += getString(R.string.sug_find_couple);
+            else
+                if(Age < 28)
+                    strSug += getString(R.string.sug_not_hurry);
+                else if(Age >= 28 && Age <= 32 )
+                    strSug += getString(R.string.sug_get_married);
+                else
+                    strSug += getString(R.string.sug_find_couple);
+
+            mtxtR.setText(strSug);
+        }
+
+    };
 }
