27e2f90b78bb00994943b6d70e55d955941a6eb9
Ancacion <joker103186@gmail.com>
2018-03-11 15:26:25 +0800

    1st homework for app

--- app/src/main/java/com/example/user/hw1/MainActivity.java
+++ app/src/main/java/com/example/user/hw1/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.user.hw1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText minputsex, minputage;
+    private Button mok;
+    private TextView mtxtR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minputsex = (EditText) findViewById(R.id.editText);
+        minputage = (EditText) findViewById(R.id.editText2);
+        mok = (Button) findViewById(R.id.button);
+        mtxtR = (TextView) findViewById(R.id.textView2);
     }
 }
