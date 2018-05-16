fc033e5a79e931a729221e5576fc7270a30dcfdc
Ancacion <joker103186@gmail.com>
2018-05-16 17:06:12 +0800

    hw8

--- app/src/main/java/com/example/user/hw8/MainActivity.java
+++ app/src/main/java/com/example/user/hw8/MainActivity.java
@@ -5,14 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,29 +30,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         spinner = findViewById(R.id.spinner);
         editDate = findViewById(R.id.editDate);
         editDate.setEnabled(false);
         editCost = findViewById(R.id.editCost);
-        ((DatePicker) findViewById(R.id.datePicker)).setOnDateChangedListener(dpkDatePicker_OnDateChanged);
+        ((DatePicker) findViewById(R.id.dpkdatePicker)).setOnDateChangedListener(dpkDatePicker_OnDateChanged);
         ((CalendarView) findViewById(R.id.calendarView)).setOnDateChangeListener(cldCalendar_OnDateChanged);
         findViewById(R.id.inputButton).setOnClickListener(inputButton_OnClick);
         findViewById(R.id.recordButton).setOnClickListener(recordButton_OnClick);
+
         stringBuilder = new StringBuilder();
         formatter = new Formatter(stringBuilder, Locale.TAIWAN);
         dataList = new ArrayList<>();
         number = 0;
     }
+
     private final DatePicker.OnDateChangedListener dpkDatePicker_OnDateChanged = new DatePicker.OnDateChangedListener() {
         @Override
         public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            //@Override
             String date = year + "/" + (monthOfYear + 1) + "/" + dayOfMonth;
             editDate.setText(date);
         }
     };
+
     private final CalendarView.OnDateChangeListener cldCalendar_OnDateChanged = new CalendarView.OnDateChangeListener() {
         @Override
         public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            //@Override
             String date = year + "/" + (month + 1) + "/" + dayOfMonth;
             editDate.setText(date);
         }
@@ -72,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener recordButton_OnClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            //@Override
             Intent intent = new Intent(MainActivity.this, RecordActivity.class);
             intent.putStringArrayListExtra("dataList", dataList);
             startActivity(intent);
